69bbbb64d28596a2e1c89d8296f0982498ed747d
Justin Tooley <jstooley823@gmail.com>
2017-10-31 00:22:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,8 +38,8 @@ class Dog
 
     else
       sql = <<-SQL
-      INSERT INTO dogs (name, breed)
-      VALUES(?, ?)
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
       SQL
       DB[:conn].execute(sql,self.name,self.breed)
       @id = DB[:conn].execute('SELECT lastinsertrowid() FROM dogs'[0][0])
