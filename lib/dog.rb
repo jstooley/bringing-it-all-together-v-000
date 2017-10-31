39d776979f3fea910332c153b0fcdf76d9d106e7
Justin Tooley <jstooley823@gmail.com>
2017-10-31 00:35:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,12 @@ class Dog
     end
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE name = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql,name).map do |row|
+    end
   end
 
 end
