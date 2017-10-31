79118b48de8a15d9683e16166882e33c51bdea4d
Justin Tooley <jstooley823@gmail.com>
2017-10-31 00:20:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,7 @@ class Dog
   end
 
   def save
-    
+
     if self.id
       self.update
       
@@ -44,6 +44,7 @@ class Dog
       DB[:conn].execute(sql,self.name,self.breed)
       @id = DB[:conn].execute('SELECT lastinsertrowid() FROM dogs'[0][0])
     end
+    
     self
   end
 end
