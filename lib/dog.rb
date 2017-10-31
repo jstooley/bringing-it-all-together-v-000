1e33f29392b0424470041cade17639cbe31ae56f
Justin Tooley <jstooley823@gmail.com>
2017-10-31 00:11:22 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(hash)
+    @name = hash[:name]
+    @breed = hash[:breed]
+    @id = hash[:id] || nil
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE IF EXISTS dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def new_from_db
+  end
+end
