7a0c8d9e955ded99388136abf1a5179164ac6dee
Justin Tooley <jstooley823@gmail.com>
2017-10-31 00:20:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,11 +32,13 @@ class Dog
   end
 
   def save
+    
     if self.id
       self.update
+      
     else
       sql = <<-SQL
-      INSERT INTO dogs( name, breed) 
+      INSERT INTO dogs( name, breed)
       VALUES ( ?, ?)
       SQL
       DB[:conn].execute(sql,self.name,self.breed)
